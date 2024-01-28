ec8e210178576d54538e006c758ba61b7cec3e26
slyzoo <gavinhimes@gmail.com>
2024-01-27 20:55:56 -0600

    redoing C

---
+++ 2_comments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    // this is a single line comment 
+
+    /* this
+    is
+    a
+    multi
+    line
+    comment */
+    return 0;
+}
---
+++ 4_variables.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main(){
+
+    // variable a thing to hold strings, integers, booleans, and floats
+
+    // declaration
+    int x;
+
+    // initialization
+    x=14;
+
+    // both declaration and initalization
+    int y = 140;
+
+    // integer
+    int age = 17;
+
+    // float
+    float gpa = 2.2;
+
+    // character
+    char grade = 'C';
+
+    // string
+    char name[] = "slyzoo"; // technically an array of characters
+
+printf("hello %s \n", name);
+
+printf("you are %d years old\n", age);
+
+printf("your grade average is %c\n", grade);
+
+printf("your gpa is %f\n", gpa);
+
+
+
+    return 0;
+}
