8ca2d294e6090abe7bd36b3fd4220ffe6d850a36
slyzoo <gavinhimes@gmail.com>
2024-04-22 14:33:28 -0500

    format specifiers

--- 5_format_specifiers.c
+++ 5_format_specifiers.c
@@ -2,17 +2,24 @@
 
 int main(){
 
-    // %.1 = decimal precision
-    // %1 = min field width
-    // %- = left align
+
+    // %c = Character
+    // %s = String
+    // %f = Float
+    // %lf = Double (Long float)
+    // %d = Integer (Digit)
+
+    // %0.1 = Decimal precision
+    // %1.0 = Minimum field width
+    // %-0 = Left align
 
     float item1 = 5.75;
     float item2 = 10.00;
     float item3 = 100.99;
 
-    printf("Item 1: $%.2f\n", item1);
-    printf("Item 2: $%.2f\n", item2);
-    printf("Item 3: $%.2f", item3);
+    printf("Item 1: $%8.2f\n", item1);
+    printf("Item 2: $%2.8f\n", item2);
+    printf("Item 3: $%2f\n", item3);
 
     return 0;
 }
